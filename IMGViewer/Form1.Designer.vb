63a63783f34a1cabd4e48c4e51ee84dd410aa4de
idabgsram <36509833+idabgsram@users.noreply.github.com>
2018-06-29 13:30:39 +0800

    first commit

---
+++ IMGViewer/Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripProgressBar1 = New System.Windows.Forms.ToolStripProgressBar()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.ToolStrip1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3})
+        Me.ListView1.Location = New System.Drawing.Point(12, 28)
+        Me.ListView1.MultiSelect = False
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(351, 387)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Filename"
+        Me.ColumnHeader1.Width = 137
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Filesize"
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Offset"
+        Me.ColumnHeader3.Width = 146
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton1, Me.ToolStripSeparator1, Me.ToolStripButton2})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(375, 25)
+        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), System.Drawing.Image)
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(81, 22)
+        Me.ToolStripButton1.Text = "Open IMG"
+        '
+        'ToolStripButton2
+        '
+        Me.ToolStripButton2.Image = CType(resources.GetObject("ToolStripButton2.Image"), System.Drawing.Image)
+        Me.ToolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton2.Name = "ToolStripButton2"
+        Me.ToolStripButton2.Size = New System.Drawing.Size(60, 22)
+        Me.ToolStripButton2.Text = "About"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripStatusLabel2, Me.ToolStripProgressBar1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 423)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(375, 22)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(39, 17)
+        Me.ToolStripStatusLabel1.Text = "Ready"
+        '
+        'ToolStripStatusLabel2
+        '
+        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(0, 17)
+        '
+        'ToolStripProgressBar1
+        '
+        Me.ToolStripProgressBar1.Name = "ToolStripProgressBar1"
+        Me.ToolStripProgressBar1.Size = New System.Drawing.Size(100, 16)
+        Me.ToolStripProgressBar1.Visible = False
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "gta3.img"
+        Me.OpenFileDialog1.Filter = "gta3.img|gta3.img"
+        Me.OpenFileDialog1.Title = "Open gta3.img"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(375, 445)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.ListView1)
+        Me.Name = "Form1"
+        Me.Text = "IMG Viewer"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripButton1 As ToolStripButton
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ToolStripButton2 As ToolStripButton
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel2 As ToolStripStatusLabel
+    Friend WithEvents ToolStripProgressBar1 As ToolStripProgressBar
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ IMGViewer/Form1.vb
@@ -0,0 +1,239 @@
+﻿Imports System.IO
+Public Class Form1
+
+    '// GTA IMG Viewer Source for VB.NET
+    '// by Idabgsram
+    '// Based from gta126705@hotmail.com vb6 source with bunch of adjustment
+
+#Region "Variable Declarations"
+    Private lngCurrentOffset As Long
+    Private imgFileName As String
+    Private imgDirPath As String
+    Private imgListPath As String
+    Private imgFileLocation As String
+    Private fileTypeTemp As String = ""
+    Private fileNameTemp As String = ""
+    Private fileSizeTemp As String = ""
+    Private fileOffsetTemp As String = ""
+    Private DirFileOpened As Boolean = False
+#End Region
+
+#Region "Function"
+    Private Function ReverseByte(strData As String) As String
+        Dim strTemp() As String
+        Dim strReturn As String
+        Dim i As Long
+        Dim y As Long
+
+        ReDim strTemp(Len(strData) / 2)
+
+        y = 1
+        strReturn = ""
+
+        For i = 0 To UBound(strTemp) - 1
+            strTemp(i) = Mid(strData, y, 2)
+            y = y + 2
+        Next i
+
+        For i = (UBound(strTemp) - 1) To 0 Step -1
+            strReturn = strReturn & strTemp(i)
+        Next i
+
+        ReverseByte = strReturn
+    End Function
+    Private Function ReadByteHex(strData As String) As String
+        Dim i As Long
+        Dim strTemp As String
+
+        strTemp = ""
+
+        For i = 1 To Len(strData)
+            If Len(Hex(Asc(Mid(strData, i, 1)))) = 1 Then
+                strTemp = strTemp & "0" & Hex(Asc(Mid(strData, i, 1)))
+            Else
+                strTemp = strTemp & Hex(Asc(Mid(strData, i, 1)))
+            End If
+        Next i
+
+        strTemp = ReverseByte(strTemp)
+
+        ReadByteHex = strTemp
+    End Function
+    Private Function GetValue(lngOffset As Long, lngSizeInByte As Integer, Optional intOption As Integer = 2) As String
+        Dim strTemp As String
+        strTemp = StrDup(lngSizeInByte, Str(&H0))
+        FileGet(1, strTemp, lngOffset + 1)
+
+        Select Case intOption
+            Case 0
+                GetValue = strTemp
+            Case 1
+                GetValue = ReadByteHex(strTemp)
+            Case 2
+                GetValue = CDbl("&H" & ReadByteHex(strTemp))
+            Case 3
+                If ReadByteHex(strTemp) = True Then GetValue = "True" Else GetValue = "False"
+            Case Else
+                GetValue = "Unknown"
+        End Select
+    End Function
+    Private Function GetIMGVersion() As Integer
+        Dim i As Integer 'counter
+
+        FileOpen(1, imgDirPath & "\" & imgFileName, OpenMode.Binary, OpenAccess.Read, OpenShare.LockRead)
+        lngCurrentOffset = 0
+
+        If GetValue(lngCurrentOffset, 4, 0) = "VER2" Then
+            GetIMGVersion = 2
+            FileClose(1)
+            Exit Function
+        End If
+        FileClose(1)
+
+        i = 0
+
+        imgListPath = imgDirPath & "\gta3.dir"
+        If DirFileOpened = True Then imgFileLocation = imgDirPath & "\gta3.img"
+
+        If File.Exists(imgListPath) = True Then
+            GetIMGVersion = 1
+            Exit Function
+        End If
+
+        GetIMGVersion = -1
+    End Function
+    Private Sub ReadIMGVersion2()
+        lngCurrentOffset = 0
+        ToolStripStatusLabel2.Visible = False
+        ToolStripStatusLabel1.Text = "Reading v2 Archive"
+        lngCurrentOffset = lngCurrentOffset + &H8
+        ToolStripProgressBar1.Value = 0
+        ListView1.BeginUpdate()
+        AddToList(GetValue(lngCurrentOffset + &H4, 4), lngCurrentOffset, True)
+        ListView1.EndUpdate()
+        ToolStripStatusLabel2.Visible = True
+        ToolStripStatusLabel2.Text = "IMG 2 - GTA SA"
+    End Sub
+    Private Sub ReadIMGVersion1()
+        lngCurrentOffset = 0
+        ToolStripStatusLabel2.Visible = False
+        ToolStripStatusLabel1.Text = "Reading v1 Archive"
+        ToolStripProgressBar1.Value = 0
+        ListView1.BeginUpdate()
+        AddToList(FileLen(imgListPath) / 32, lngCurrentOffset, True)
+        ListView1.EndUpdate()
+        ToolStripStatusLabel2.Visible = True
+        ToolStripStatusLabel2.Text = "IMG 1 - GTA 3 / VC"
+    End Sub
+    Private Sub AddToList(intFileNumber As Integer, offsetNumber As String, isVersionOne As Boolean)
+        On Error Resume Next
+        ToolStripProgressBar1.Visible = True
+        Dim i As Integer
+        Select Case isVersionOne
+            Case True
+                For i = 0 To (intFileNumber - 1)
+                    fileNameTemp = GetValue(lngCurrentOffset + &H8, 24, 0)
+                    fileTypeTemp = Strings.Right(fileTypeTemp, 3)
+                    fileSizeTemp = ((CDbl(GetValue(lngCurrentOffset + &H4, 4)) * 2048) / 1024) & " kb"
+                    offsetNumber = Hex(CDbl(GetValue(lngCurrentOffset, 4)) * 2048)
+                    Select Case offsetNumber.Length
+                        Case 1
+                            fileOffsetTemp = StrDup(7, "0") & offsetNumber
+                        Case 2
+                            fileOffsetTemp = StrDup(6, "0") & offsetNumber
+                        Case 3
+                            fileOffsetTemp = StrDup(5, "0") & offsetNumber
+                        Case 4
+                            fileOffsetTemp = StrDup(4, "0") & offsetNumber
+                        Case 5
+                            fileOffsetTemp = StrDup(3, "0") & offsetNumber
+                        Case 6
+                            fileOffsetTemp = StrDup(2, "0") & offsetNumber
+                        Case 7
+                            fileOffsetTemp = StrDup(1, "0") & offsetNumber
+                        Case 8
+                            fileOffsetTemp = offsetNumber
+                        Case Else
+                            fileOffsetTemp = offsetNumber
+                    End Select
+
+                    With ListView1.Items.Add(fileNameTemp)
+                        .SubItems.Add(fileSizeTemp)
+                        .SubItems.Add(fileOffsetTemp)
+                        lngCurrentOffset = lngCurrentOffset + 32
+                    End With
+                    Application.DoEvents()
+                    ToolStripStatusLabel1.Text = "Reading file(s)..."
+                    ToolStripProgressBar1.Value = 100 / (intFileNumber) * (i + 1)
+                Next i
+            Case False
+                For i = 0 To (intFileNumber - 1)
+                    fileNameTemp = GetValue(lngCurrentOffset + &H8, 24, 0)
+                    fileTypeTemp = Strings.Right(fileTypeTemp, 3)
+                    fileSizeTemp = ((CDbl(GetValue(lngCurrentOffset + &H4, 4)) * 2048) / 1024) & " kb"
+                    fileOffsetTemp = Hex(CDbl(GetValue(lngCurrentOffset, 4)) * 2048)
+                    With ListView1.Items.Add(fileNameTemp)
+                        .SubItems.Add(fileSizeTemp)
+                        .SubItems.Add(fileOffsetTemp)
+                        lngCurrentOffset = lngCurrentOffset + 32
+                    End With
+                    Application.DoEvents()
+                    ToolStripStatusLabel1.Text = "Reading file(s)..."
+                    ToolStripProgressBar1.Value = 100 / (intFileNumber) * (i + 1)
+                Next i
+        End Select
+        If intFileNumber <= 1 Then
+            ToolStripStatusLabel1.Text = "Loaded " & intFileNumber & " file"
+        Else
+            ToolStripStatusLabel1.Text = "Loaded " & intFileNumber & " files"
+        End If
+        ToolStripProgressBar1.Visible = False
+    End Sub
+    Private Sub OpenIMG(imgFilePath As String)
+        DirFileOpened = False
+        imgDirPath = Path.GetDirectoryName(imgFilePath)
+        Select Case Strings.Right(imgFilePath, 3).ToLower
+            Case "dir"
+                If File.Exists(imgDirPath & "\gta3.img") = True Then
+                    DirFileOpened = True
+
+                Else
+                    MsgBox("gta3.img not found, cannot process gta3.dir", vbExclamation, "Error")
+                    Exit Sub
+                End If
+            Case Else : DirFileOpened = False
+        End Select
+        imgFileName = Path.GetFileName(imgFilePath)
+
+        ListView1.Items.Clear()
+
+        Select Case GetIMGVersion()
+            Case 2 'GTA SA
+                FileOpen(1, imgFilePath, OpenMode.Binary, OpenAccess.Read)
+                ReadIMGVersion2()
+                FileClose(1)
+            Case 1 'GTA 3 and VC
+                FileOpen(1, imgListPath, OpenMode.Binary, OpenAccess.Read)
+                ReadIMGVersion1()
+                FileClose(1)
+            Case Else
+                MsgBox("Not a valid GTA-IMG file", vbExclamation, "Error")
+        End Select
+    End Sub
+#End Region
+
+#Region "Button"
+    Private Sub ToolStripButton2_Click(sender As Object, e As EventArgs) Handles ToolStripButton2.Click
+        MsgBox("GTA IMG Viewer" & vbCrLf & Application.ProductVersion & vbCrLf & vbCrLf & "by Idabgsram")
+    End Sub
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+        Select Case OpenFileDialog1.ShowDialog()
+            Case DialogResult.Cancel
+                Exit Sub
+            Case DialogResult.No
+                Exit Sub
+        End Select
+        OpenIMG(OpenFileDialog1.FileName.ToString)
+    End Sub
+#End Region
+End Class
